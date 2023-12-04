b79cdeeb4cfdd59c77f2609a0e20efc7f064456d
Anamol <anmoldkl971@gmail.com>
2023-12-04 23:40:13 +0545

    version 1.0

--- GFinanceOAuth.py
+++ GFinanceOAuth.py
@@ -5,3 +5,10 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+
+app = FastAPI()
+
+SECRET_KEY = "hbGciOiJIUzI1NiIsInRbdWIiOiJ4em5vbSIsImV4cCI6MTcwMDk"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+
