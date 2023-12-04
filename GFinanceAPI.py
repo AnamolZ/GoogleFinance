a2eefa07ef5b70647d73dc4e5242572d5fecdeda
Anamol <anmoldkl971@gmail.com>
2023-12-04 23:37:43 +0545

    version 1.0

--- GFinanceAPI.py
+++ GFinanceAPI.py
@@ -9,6 +9,10 @@ from fastapi import HTTPException
 
 from GFinanceAPI import get_current_user
 
-
+data={}
+async def fetch_data(client, semaphore, stock):
+    start_time = time.time()
+    async with semaphore:
+        
 
 
